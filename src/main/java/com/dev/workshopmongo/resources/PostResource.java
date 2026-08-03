6557d04a37461208a2de43dd1f966dd510816cf8
Patrick Borges <patrickpborges1996@gmail.com>
2026-08-03 18:23:07 -0300

    Simple query using query methods

--- src/main/java/com/dev/workshopmongo/resources/PostResource.java
+++ src/main/java/com/dev/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.dev.workshopmongo.resources;
 import com.dev.workshopmongo.domain.Post;
 import com.dev.workshopmongo.domain.User;
 import com.dev.workshopmongo.dto.UserDTO;
+import com.dev.workshopmongo.resources.util.URL;
 import com.dev.workshopmongo.services.PostService;
 import com.dev.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
